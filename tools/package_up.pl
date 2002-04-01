d8b19c27d6c8279812cb5aa6ae3c7bf585eb55a7
Shay Harding <sharding@ccbill.com>
2002-04-01 18:36:58 +0000

    Import of SHAYH/Term-StatusBar-1.01 from CPAN.
    gitpan-cpan-distribution: Term-StatusBar
    gitpan-cpan-version:      1.01
    gitpan-cpan-path:         SHAYH/Term-StatusBar-1.01.tar.gz
    gitpan-cpan-author:       SHAYH
    gitpan-cpan-maturity:     released

--- tools/package_up.pl
+++ tools/package_up.pl
@@ -1,16 +1,19 @@
 #!/usr/bin/perl
 
-  my $ver = "1.00";
-  `./build_manifest.pl`;
+  my $pkg = 'Term-StatusBar';
+  my $ver = "1.01";
+  unlink("../$pkg-$ver.tar.gz");
+  `./build_manifest.pl; ./build_readme.pl`;
 
   open FILE, "../MANIFEST";
   my $str;
 
   for (<FILE>){
       chomp;
-      $str .= "./Term-StatusBar-$ver/$_ ";
+      $str .= "./$pkg/$_ ";
   }
 
-  `tar -C ../.. -zcf Term-StatusBar-$ver.tar.gz $str`;
-  `mv Term-StatusBar-$ver.tar.gz ../`;
+  `tar -C ../.. -zcf $pkg-$ver.tar.gz $str`;
+  `mv $pkg-$ver.tar.gz ../`;
   close FILE;
+
